e20c23c14d9c2c077f70977d26a5283e22939687
ezemaut <106409811+ezemaut@users.noreply.github.com>
2023-05-23 18:24:01 -0300

    Add files via upload

--- TD/validacion.py
+++ TD/validacion.py
@@ -1,12 +1,6 @@
-
-
-def validacion(inp:list): 
-    ips = []
-    for input in inp:
-        ips.append(input.split(':')[1])
-
+def val(pos):
     nums = []
-    for ip in ips:
+    for ip in pos:
         nums = ip.split('.')
         if len(nums) == 4:
             for number in nums:
@@ -15,11 +9,24 @@ def validacion(inp:list):
                      return False
                 else: return False
         else: return False
-                    
     return True
+
+
+
+def validacion(inp:list): 
+    ips = []
+    for input in inp:
+        ips.append(input.split(':')[1])
+    return val(ips)
+
+    
+                    
+    
 
 ip = ['algo:1.1.1.1']
 ips = ['algo:1.1.1.1','otro:2.1.2.2']
+s = ['3.3.3.3.3']
 
 
 print(validacion(ips))
+print(val(s))
