9b84140f8da65003c353afc5181664ea42da4b1e
ezemaut <106409811+ezemaut@users.noreply.github.com>
2023-05-30 18:08:41 -0300

    funca, falta argsparse

--- TD/server.py
+++ TD/server.py
@@ -11,26 +11,15 @@ DNS_SERVER_IP = "127.0.0.1"  # Your local IP
 
 def forward_dns(og, orig_pkt: IP, addr):
     print(f"Forwarding: {orig_pkt[DNSQR].qname}")
-    # response = sr1(
-    #     IP(dst='8.8.8.8')/
-    #         UDP(sport=orig_pkt[UDP].sport)/
-    #         DNS(rd=1, id=orig_pkt[DNS].id, qd=DNSQR(qname=orig_pkt[DNSQR].qname)),
-    #     verbose=0,
-    # )
 
-    fwd_pkt = IP(dst='8.8.8.8')/UDP(sport=orig_pkt[UDP].sport, dport = 53)/DNS(og)
-    print(fwd_pkt.show())
+    fwd_pkt = DNS(og)
+
     socket_google.sendto(bytes(fwd_pkt),('8.8.8.8',530))
     response, r_addr = s.recvfrom(512)
 
-    response = IP()/UDP()/DNS(response)
-    # print(response.show())
-
-    resp_pkt = IP(dst=orig_pkt[IP].src, src=DNS_SERVER_IP)/UDP(dport=orig_pkt[UDP].sport)/DNS()
-    resp_pkt[DNS] = response[DNS]
-
+    response = DNS(response)
     
-    s.sendto(bytes(resp_pkt), addr)
+    s.sendto(bytes(response), addr)
     return f"Responding to {orig_pkt[IP].src}"
 
 def get_response(og, pkt: IP, addr):
@@ -39,10 +28,11 @@ def get_response(og, pkt: IP, addr):
         pkt[DNS].opcode == 0 and
         pkt[DNS].ancount == 0
     ):
-        if "t.com" in str(pkt["DNS Question Record"].qname):
+        if "google.com" in str(pkt["DNS Question Record"].qname):
             print("hay spoof")
-            spf_resp = IP(dst=pkt[IP].src)/UDP(dport=pkt[UDP].sport, sport=53)/DNS(id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname, rdata=DNS_SERVER_IP)/DNSRR(rrname="google.com",rdata=DNS_SERVER_IP))
-            # send(spf_resp, verbose=0)
+
+            spf_resp = DNS(qr=1,id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname, rdata='1.2.3.4')/DNSRR(rrname="google.com",rdata=DNS_SERVER_IP))
+            spf_resp["DNS Question Record"].qname = "google.com"
             s.sendto(bytes(spf_resp), addr)
             return f"Spoofed DNS Response Sent: {pkt[IP].src}"
         else:
@@ -53,9 +43,7 @@ def get_response(og, pkt: IP, addr):
 while True:
     message, addr = s.recvfrom(512)
     msg = IP()/UDP()/DNS(message)
-    # print(msg.show())
-    # print(msg.show())
     print(get_response(message, msg, addr))
-    break
+    # break
     
-    
+    #CERRAR SOCKET
