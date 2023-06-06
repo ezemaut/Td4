f753be319fe04c4819c46df53ef55fac65ae8999
ezemaut <106409811+ezemaut@users.noreply.github.com>
2023-06-06 13:49:13 -0300

    Add files via upload

--- htt.py
+++ htt.py
@@ -1,6 +1,12 @@
 
 #curl http://www.google.com --resolve www.google.com:80:127.0.0.1
 
+# -r www.google.com:www.netflix.com
+
+# solo que haga 1 respone?
+# maximo 1 archivo?
+#como probar
+
 import argparse
 import socket
 from scapy.all import DNS, DNSQR, DNSRR, IP, TCP, Raw, ls, Ether
@@ -51,21 +57,27 @@ def necesita_archivo() -> bool:
         print("necesita archivo")
     return rv
 
-def necesita_redireccionar() -> bool:
-    rv = False
-    if host in dic_Pedido_Respueta.keys():
-            rv = True
-            print("necesita redi")
-    return rv
+def build_a_packet_200():
+    with open(archivo, 'r') as file:
+        html_content = file.read()
 
-def build_a_packet():
-    destino = dic_Pedido_Respueta[host]
+    response = "HTTP/1.1 200 OK\r\n"
+    response += "Content-Type: text/html\r\n"
+    response += "\r\n"
+    response += html_content
+    return response
+
+def build_a_packet_301():
+    if host in dic_Pedido_Respueta.keys():
+        destino = dic_Pedido_Respueta[host]
+    else: destino = host
+    
     http_response = (
         "HTTP/1.1 301 Moved Permanently\r\n"
         f"Location: https://{destino}\r\n"
         "\r\n")
-    res = bytes_encode(http_response)
-    res_pkt = res
+    res_pkt = bytes_encode(http_response)
+    print(f'Respondiendo redirección hacia https:/{destino}')
     return res_pkt
 
 socket_local = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -76,29 +88,20 @@ socket_local.listen()
 conn, addr = socket_local.accept()
 
 print(f"Connected by {addr}")
-while True:
-    message = conn.recv(1024)
-    # mirar data
-    # si data esta en d -> mandar html
-    # si data esta en r -> rediriccionar
-    # no esta ninguno -> mandar a donde pide
-
-    scapy_pkt = HTTP(message)
-
-    if packet_is_good(scapy_pkt):
-        
-        if necesita_archivo():
-            # mandar archivo
-            pass
-
-        elif necesita_redireccionar():
-            res = build_a_packet()  
-        # else: mandar()
-   
-   
-   
-    if not res:
-        print('not message')
-        break
-    print(2)
-    conn.sendall(res)
+# while True:
+message = conn.recv(1024)
+# mirar data
+# si data esta en d -> mandar html
+# si data esta en r -> rediriccionar
+# no esta ninguno -> mandar a donde pide
+scapy_pkt = HTTP(message)
+if packet_is_good(scapy_pkt):
+    
+    if necesita_archivo():
+        res =build_a_packet_200()
+    else: res =build_a_packet_301()
+         
+if not res:
+    print('not message')
+    # break
+conn.sendall(res)
